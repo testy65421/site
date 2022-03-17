4de8dfd75b9f2569d8e9fb10fdb003c2606bb647
testy65421 <95231107+testy65421@users.noreply.github.com>
2022-03-17 09:46:55 +0000

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+
+}
+/*
+     FILE ARCHIVED ON 00:19:22 Feb 12, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 09:45:24 Mar 17, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 67.709
+  exclusion.robots: 0.21
+  exclusion.robots.policy: 0.201
+  RedisCDXSource: 0.756
+  esindex: 0.011
+  LoadShardBlock: 47.151 (3)
+  PetaboxLoader3.datanode: 58.966 (4)
+  CDXLines.iter: 16.248 (3)
+  load_resource: 72.695
+  PetaboxLoader3.resolve: 31.806
+*/
